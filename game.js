2651a2a498337b448868c4c6990ec647fb02b3dc
Ibrahim fariat <ibrahimfariat@gmail.com>
2021-04-09 19:00:02 -0400

    refactored code a little bit

--- game.js
+++ game.js
@@ -6,14 +6,49 @@ function resolution(width = 400, height = 400) {
     ctx.canvas.width = width;
     ctx.canvas.height = height;
 }
-let snake, snakeLength, food, currentHue;
-
-function initialize() {
-    resolution(600, 600);
+let pubVars = {
+    snake: undefined,
+    snakeLength: undefined,
+    food: undefined,
+    currentHue: undefined,
+    fractions: undefined
+}
+let helpers = {
+    collision(isSelfCol, args) {
+        if (isSelfCol) {
+            if (args.x == pubVars.food.x && args.y == pubVars.food.y) {
+                pubVars.food.respawnFood();
+                tails.push(new Snake(pubVars.snakeLength - 1, "tail"));
+                pubVars.snakeLength++;
+                pubVars.snake.delay - 0.2;
+            }
+        } else {
+            for (let i = 0; i < historyPath.length; i++) {
+                if (args.x == historyPath[i].x && args.y == historyPath[i].y) {
+                    alert('self-collision detected');
+                }
+            }
+        }
+    },
+    randHue() {
+        let randH = Math.floor(Math.random() * 255);
+        currentHue = randH;
+        return randH;
+    },
+    positionLogger(limit, loc) {
+        historyPath.push(loc);
+        if (historyPath.length > limit) {
+            historyPath.shift();
+        }
+    }
+}
+function setup() {
+    resolution();
     input.listen();
-    snakeLength = 2;
-    snake = new Snake('head');
-    food = new Food();
+    pubVars.fractions = 16;
+    pubVars.snakeLength = 2;
+    pubVars.snake = new Snake("head");
+    pubVars.food = new Food();
     loop();
 }
 let input = {
@@ -22,48 +57,52 @@ let input = {
     right: true,
     up: false,
     listen() {
-        addEventListener('keydown', e => {
-            switch (e.key) {
-                case "ArrowLeft":
-                    this.left = true;
-                    this.down = false;
-                    this.right = false;
-                    this.up = false;
-                    break;
-                case "ArrowRight":
-                    this.left = false;
-                    this.down = false;
-                    this.right = true;
-                    this.up = false;
-                    break;
-                case "ArrowUp":
-                    this.left = false;
-                    this.down = false;
-                    this.right = false;
-                    this.up = true;
-                    break;
-                case "ArrowDown":
-                    this.left = false;
-                    this.down = true;
-                    this.right = false;
-                    this.up = false;
-                    break;
-                default:
-                    break;
-            }
-        }, false)
+        addEventListener(
+            "keydown",
+            (e) => {
+                switch (e.key) {
+                    case "ArrowLeft":
+                        this.left = true;
+                        this.down = false;
+                        this.right = false;
+                        this.up = false;
+                        break;
+                    case "ArrowRight":
+                        this.left = false;
+                        this.down = false;
+                        this.right = true;
+                        this.up = false;
+                        break;
+                    case "ArrowUp":
+                        this.left = false;
+                        this.down = false;
+                        this.right = false;
+                        this.up = true;
+                        break;
+                    case "ArrowDown":
+                        this.left = false;
+                        this.down = true;
+                        this.right = false;
+                        this.up = false;
+                        break;
+                    default:
+                        break;
+                }
+            },
+            false
+        );
     }
-}
+};
 
 class Snake {
     constructor(i, type) {
-        this.x = type == 'tail' ? historyPath[i].x : 0;
-        this.y = type == 'tail' ? historyPath[i].y : 0;
+        this.x = type == "tail" ? historyPath[i].x : 0;
+        this.y = type == "tail" ? historyPath[i].y : 0;
         this.type = type;
         this.index = i;
         this.delay = 10;
         this.localDelay = 10;
-        this.size = ctx.canvas.width / 24;
+        this.size = ctx.canvas.width / pubVars.fractions;
         this.color = "white";
     }
     draw() {
@@ -79,61 +118,52 @@ class Snake {
             } else {
                 this.localDelay = this.delay;
                 if (input.left) {
-                    this.x -= ctx.canvas.width / 24;
+                    this.x -= ctx.canvas.width / pubVars.fractions;
                 }
                 if (input.right) {
-                    this.x += ctx.canvas.width / 24;
+                    this.x += ctx.canvas.width / pubVars.fractions;
                 }
                 if (input.up) {
-                    this.y -= ctx.canvas.width / 24;
+                    this.y -= ctx.canvas.width / pubVars.fractions;
                 }
                 if (input.down) {
-                    this.y += ctx.canvas.width / 24;
+                    this.y += ctx.canvas.width / pubVars.fractions;
                 }
-                // bounding box => 
-                if (this.x + ctx.canvas.width / 24 > ctx.canvas.width) {
+                // bounding box =>
+                if (this.x + ctx.canvas.width / pubVars.fractions > ctx.canvas.width) {
                     this.x = 0;
                 }
-                if (this.y + ctx.canvas.height / 24 > ctx.canvas.width) {
+                if (this.y + ctx.canvas.height / pubVars.fractions > ctx.canvas.width) {
                     this.y = 0;
                 }
                 if (this.y < 0) {
-                    this.y = ctx.canvas.height - ctx.canvas.height / 24;
+                    this.y = ctx.canvas.height - ctx.canvas.height / pubVars.fractions;
                 }
                 if (this.x < 0) {
-                    this.x = ctx.canvas.width - ctx.canvas.width / 24;
+                    this.x = ctx.canvas.width - ctx.canvas.width / pubVars.fractions;
                 }
-                collision({ ...this });
-                historyKeeper(snakeLength, { x: this.x, y: this.y })
+                helpers.collision(true, { ...this });
+                helpers.collision(false, { ...this });
+                helpers.positionLogger(pubVars.snakeLength, { x: this.x, y: this.y });
             }
         } else {
             this.localDelay--;
         }
     }
+}
 
-}
-function collision(snakeParam) {
-    if (snakeParam.x == food.x && snakeParam.y == food.y) {
-        food.respawnFood();
-        tails.push(new Snake(snakeLength - 1, 'tail'))
-        snakeLength++;
-        snake.delay - 0.2;
-    }
-}
-function randomHue() {
-    let randH = Math.floor(Math.random() * 255);
-    currentHue = randH;
-    return randH;
-}
+
 class Food extends Snake {
     constructor() {
         super();
-        this.x = (Math.floor(Math.random() * 24)) * ctx.canvas.width / 24;
-        this.y = (Math.floor(Math.random() * 24)) * ctx.canvas.height / 24;
-        this.color = `hsl(${randomHue()}, 100%, 80%)`;
+        this.x =
+            (Math.floor(Math.random() * pubVars.fractions) * ctx.canvas.width) / pubVars.fractions;
+        this.y =
+            (Math.floor(Math.random() * pubVars.fractions) * ctx.canvas.height) / pubVars.fractions;
+        this.color = `hsl(${helpers.randHue()}, 100%, 80%)`;
     }
     draw() {
-        ctx.save()
+        ctx.save();
         ctx.shadowColor = `hsl(${currentHue}, 100%, 50%)`;
         ctx.shadowBlur = 50;
         ctx.fillStyle = this.color;
@@ -144,31 +174,26 @@ class Food extends Snake {
         this.draw();
     }
     respawnFood() {
-        this.color = `hsl(${randomHue()}, 100%, 50%)`;
-        this.x = ((Math.floor(Math.random() * 24)) * ctx.canvas.width / 24);
-        this.y = ((Math.floor(Math.random() * 24)) * ctx.canvas.height / 24);
+        this.color = `hsl(${helpers.randHue()}, 100%, 50%)`;
+        this.x =
+            (Math.floor(Math.random() * pubVars.fractions) * ctx.canvas.width) / pubVars.fractions;
+        this.y =
+            (Math.floor(Math.random() * pubVars.fractions) * ctx.canvas.height) / pubVars.fractions;
     }
 }
-let historyPath = []
+let historyPath = [];
 
-function historyKeeper(limit, loc) {
-    historyPath.push(loc);
-    if (historyPath.length > limit) {
-        historyPath.shift();
-    }
-}
-let tails = []
+let tails = [];
 function loop() {
     requestAnimationFrame(loop);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    snake.update();
+    pubVars.snake.update();
     if (tails.length) {
         for (let i = 0; i < tails.length; i++) {
             tails[i].update();
         }
     }
-    food.update();
-};
+    pubVars.food.update();
+}
 
-
-initialize();
+setup();
